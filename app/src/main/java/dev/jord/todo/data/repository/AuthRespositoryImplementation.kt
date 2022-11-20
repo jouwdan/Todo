ec9de1ce0c9062f8e66c62d7c4289979c69b6bd5
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-11-20 14:09:03 +0000

    auth repository implementation & constants

--- app/src/main/java/dev/jord/todo/data/repository/AuthRespositoryImplementation.kt
+++ app/src/main/java/dev/jord/todo/data/repository/AuthRespositoryImplementation.kt
@@ -2,9 +2,14 @@ package dev.jord.todo.data.repository
 
 import android.content.SharedPreferences
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.gson.Gson
 import dev.jord.todo.data.model.User
+import dev.jord.todo.util.FireStoreCollection
+import dev.jord.todo.util.SharedPrefConstants
 import dev.jord.todo.util.UiState
 
 class AuthRespositoryImplementation(
@@ -20,30 +25,116 @@ class AuthRespositoryImplementation(
         user: User,
         result: (UiState<String>) -> Unit
     ) {
-        TODO("Not yet implemented")
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+            if(it.isSuccessful) {
+                user.id = it.result.user?.uid ?: ""
+                updateUserInfo(user) { state ->
+                    when(state) {
+                        is UiState.Success -> {
+                            storeSession(id = it.result.user?.uid ?: "") {
+                                if(it == null) {
+                                    result.invoke(UiState.Failure("Registration successful, Failed to store session."))
+                                } else {
+                                    result.invoke(UiState.Success("Registration Successful!"))
+                                }
+                            }
+                        }
+                        is UiState.Failure -> {
+                            result.invoke(UiState.Failure(state.error))
+                        }
+                        else -> {
+
+                        }
+                    }
+                }
+            } else {
+                try {
+                    throw it.exception ?: java.lang.Exception("Invalid authentication")
+                } catch (e: FirebaseAuthWeakPasswordException) {
+                    result.invoke(UiState.Failure("Authentication failed, please use a stronger password"))
+                } catch (e: FirebaseAuthInvalidCredentialsException) {
+                    result.invoke(UiState.Failure("Authentication failed, email invalid"))
+                } catch (e: FirebaseAuthUserCollisionException) {
+                    result.invoke(UiState.Failure("Authentication failed, email already exists"))
+                } catch (e: Exception) {
+                    result.invoke(UiState.Failure(e.message))
+                }
+            }
+        } .addOnFailureListener {
+            result.invoke(
+                UiState.Failure(
+                    it.localizedMessage
+                )
+            )
+        }
     }
 
     override fun loginUser(email: String, password: String, result: (UiState<String>) -> Unit) {
-        TODO("Not yet implemented")
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                storeSession(id = task.result.user?.uid ?: "") {
+                    if (it == null) {
+                        result.invoke(UiState.Failure("Failed to store session locally"))
+                    } else {
+                        result.invoke(UiState.Success("Login Success"))
+                    }
+                }
+            }
+        }.addOnFailureListener {
+            result.invoke(UiState.Failure("Authentication failed"))
+        }
     }
 
     override fun forgotPassword(email: String, result: (UiState<String>) -> Unit) {
-        TODO("Not yet implemented")
+        auth.sendPasswordResetEmail(email).addOnCompleteListener{ task ->
+            if (task.isSuccessful) {
+                result.invoke(UiState.Success("Password reset email has been sent"))
+            } else {
+                result.invoke(UiState.Failure(task.exception?.message))
+            }
+        }.addOnFailureListener {
+            result.invoke(UiState.Failure("Failed, email does not exist"))
+        }
     }
 
     override fun logout(result: () -> Unit) {
-        TODO("Not yet implemented")
+        auth.signOut()
+        appPreferences.edit().putString(SharedPrefConstants.USER_SESSION,null).apply()
+        result.invoke()
     }
 
     override fun getSession(result: (User?) -> Unit) {
-        TODO("Not yet implemented")
+        val userSession = appPreferences.getString(SharedPrefConstants.USER_SESSION,null)
+        if (userSession == null){
+            result.invoke(null)
+        }else{
+            val user = gson.fromJson(userSession,User::class.java)
+            result.invoke(user)
+        }
     }
 
     override fun storeSession(id: String, result: (User?) -> Unit) {
-        TODO("Not yet implemented")
+        database.collection(FireStoreCollection.USER).document(id).get()
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val user = it.result.toObject(User::class.java)
+                    appPreferences.edit().putString(SharedPrefConstants.USER_SESSION,gson.toJson(user)).apply()
+                    result.invoke(user)
+                } else {
+                    result.invoke(null)
+                }
+            }.addOnFailureListener {
+                result.invoke(null)
+            }
     }
 
     override fun updateUserInfo(user: User, result: (UiState<String>) -> Unit) {
-        TODO("Not yet implemented")
+        val document = database.collection(FireStoreCollection.USER).document(user.id)
+        document.set(user)
+            .addOnSuccessListener {
+                result.invoke(UiState.Success("User has been update successfully"))
+                }.addOnFailureListener {
+                    result.invoke(UiState.Failure(it.localizedMessage))
+                }
     }
 }
---
+++ app/src/main/java/dev/jord/todo/util/Constants.kt
@@ -0,0 +1,9 @@
+package dev.jord.todo.util
+
+object SharedPrefConstants {
+    val USER_SESSION = "user_session"
+}
+
+object FireStoreCollection {
+    val USER = "user"
+}
