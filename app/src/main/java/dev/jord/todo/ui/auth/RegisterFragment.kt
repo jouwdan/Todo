ebdab28776c0e66faeca295ea66375ac592e4114
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-11-21 09:28:44 +0000

    register fragment

--- app/src/main/java/dev/jord/todo/ui/auth/RegisterFragment.kt
+++ app/src/main/java/dev/jord/todo/ui/auth/RegisterFragment.kt
@@ -5,14 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+import dev.jord.todo.R
+import dev.jord.todo.data.model.User
 import dev.jord.todo.databinding.FragmentRegisterBinding
+import dev.jord.todo.util.*
 
+@AndroidEntryPoint
 class RegisterFragment : Fragment() {
 
+    val TAG: String = "RegisterFragment"
     private var _binding: FragmentRegisterBinding? = null
-
     private val binding get() = _binding!!
+    val viewModel: AuthViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +31,75 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observer()
+        binding.register.setOnClickListener {
+            if (validation()){
+                viewModel.register(
+                    email = binding.email.text.toString(),
+                    password = binding.password.text.toString(),
+                    user = getUserObj()
+                )
+            }
+        }
+    }
+
+    fun observer() {
+        viewModel.register.observe(viewLifecycleOwner) { state ->
+            when(state){
+                is UiState.Loading -> {
+                    binding.register.setText("")
+                    binding.loading.show()
+                }
+                is UiState.Failure -> {
+                    binding.register.setText("Register")
+                    binding.loading.hide()
+                    snackbar("Registration error")
+                }
+                is UiState.Success -> {
+                    binding.register.setText("Register")
+                    binding.loading.hide()
+                    snackbar(state.data)
+                    findNavController().navigate(R.id.action_Register_to_Welcome)
+                }
+            }
+        }
+    }
+
+    fun validation(): Boolean {
+        var isValid = true
+
+        if (binding.name.text.isNullOrEmpty()){
+            isValid = false
+            snackbar(getString(R.string.enter_name))
+        }
+
+        if (binding.email.text.isNullOrEmpty()){
+            isValid = false
+            snackbar(getString(R.string.enter_email))
+        }else{
+            if (!binding.email.text.toString().isValidEmail()){
+                isValid = false
+                snackbar(getString(R.string.invalid_email))
+            }
+        }
+        if (binding.password.text.isNullOrEmpty()){
+            isValid = false
+            snackbar(getString(R.string.enter_password))
+        }else{
+            if (binding.password.text.toString().length < 8){
+                isValid = false
+                snackbar(getString(R.string.invalid_password))
+            }
+        }
+        return isValid
+    }
+
+    fun getUserObj(): User {
+        return User(
+            id = "",
+            name = binding.name.text.toString(),
+            email = binding.email.text.toString(),
+        )
     }
 
     override fun onDestroyView() {
