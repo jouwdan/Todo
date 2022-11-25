819dece444c48e0bc42613a15bdae3a62e4cd218
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-11-25 21:01:10 +0000

    model updates & comments

--- app/src/main/java/dev/jord/todo/data/model/User.kt
+++ app/src/main/java/dev/jord/todo/data/model/User.kt
@@ -1,7 +1,7 @@
 package dev.jord.todo.data.model
 
 data class User(
-    var id: String = "",
-    val name: String = "",
-    val email: String = "",
+    var id: String = "", // This is the id of the user
+    val name: String = "", // This is the name of the user
+    val email: String = "", // This is the email of the user
 )
