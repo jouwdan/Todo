ccd399c68894be6a08e0c01594214be0f2c115f2
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-11-25 20:48:50 +0000

    add logout function

--- app/src/main/java/dev/jord/todo/ui/account/AccountFragment.kt
+++ app/src/main/java/dev/jord/todo/ui/account/AccountFragment.kt
@@ -1,21 +1,30 @@
 package dev.jord.todo.ui.account
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import dagger.hilt.android.AndroidEntryPoint
+import dev.jord.todo.LoginActivity
+import dev.jord.todo.MainActivity
 import dev.jord.todo.R
 import dev.jord.todo.databinding.FragmentAccountBinding
+import dev.jord.todo.ui.auth.AuthViewModel
 
+@AndroidEntryPoint
 class AccountFragment : Fragment() {
 
     private var _binding: FragmentAccountBinding? = null
     private val binding get() = _binding!!
     val user = Firebase.auth.currentUser
+    val viewModel: AuthViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,5 +39,11 @@ class AccountFragment : Fragment() {
 
         val textView = requireView().findViewById(R.id.nameTextField) as TextView
         textView.text = "Hello, " + user?.email
+
+        binding.logoutButton.setOnClickListener {
+            viewModel.logout {
+                startActivity(Intent(requireContext(), LoginActivity::class.java))
+            }
+        }
     }
 }
