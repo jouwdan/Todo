2bd4837be2534e93f88fcf1c8de1a9ae21edb173
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-11-26 17:25:25 +0000

    vals to vars

--- app/src/main/java/dev/jord/todo/data/model/Task.kt
+++ app/src/main/java/dev/jord/todo/data/model/Task.kt
@@ -7,10 +7,10 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Task(
     @DocumentId var id: String = "", // This is the id of the document in the database
-    val title: String = "", // This is the title of the task
-    val description: String = "", // This is the description of the task
-    val priority: String = "", // Low, Medium, High
-    val dueDate: String = "", // 2021-01-01
-    val location: String = "", // 123.123, 123.123
-    val completed: Boolean = false // true or false
+    var title: String = "", // This is the title of the task
+    var description: String = "", // This is the description of the task
+    var priority: String = "", // Low, Medium, High
+    var dueDate: String = "", // 2021-01-01
+    var location: String = "", // 123.123, 123.123
+    var completed: Boolean = false // true or false
 ) : Parcelable
