e60ae9c0a9b74f18eccc65893e8a62c360a29a09
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-11-26 00:05:56 +0000

    cleanup

--- app/src/main/java/dev/jord/todo/ui/home/HomeFragment.kt
+++ app/src/main/java/dev/jord/todo/ui/home/HomeFragment.kt
@@ -6,40 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import dev.jord.todo.R
+import dev.jord.todo.data.model.Task
 import dev.jord.todo.databinding.FragmentHomeBinding
-import dev.jord.todo.ui.auth.AuthViewModel
-import dev.jord.todo.ui.auth.ForgotPasswordFragment
+import dev.jord.todo.util.UiState
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
-
-    val viewModel: TaskViewModel by viewModels()
+    private lateinit var binding: FragmentHomeBinding
+    private val viewModel: TaskViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.addTaskButton.setOnClickListener() {
+        binding.addTaskButton.setOnClickListener {
             activity?.supportFragmentManager?.beginTransaction()
                 ?.replace(R.id.container, AddTaskFragment())
                 ?.commit();
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
